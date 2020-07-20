f395244e9b53f79e1c271833a36f475408d1d7fc
Brijesh Patel <bijupat@users.noreply.github.com>
2020-07-20 21:10:10 +0530

    first commit
    header and contact us and inquiry pages done

---
+++ extend.js
@@ -0,0 +1,42 @@
+document.write('\
+\
+<div class="topfloat">\
+<div class="container">\
+  <div class="image"><img src="HOSPIMAX.jpg" width="316" height= "80" alt="Hospimax Consultancy"  title=" Hospimax Consultancy"></div>\
+  <div>\
+\
+<ul class="topnav">\
+  <li><a class="active" href="index.html">Home</a></li>\
+  <li class="dropdown">\
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>\
+    <div class="dropdown-content">\
+      <a href="layout.html">Link 1</a>\
+      <a href="#">Link 2</a>\
+      <a href="#">Link 3</a>\
+    </div>\
+  </li>\
+  <li class="dropdown">\
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>\
+    <div class="dropdown-content">\
+      <a href="error.html">Link 1</a>\
+      <a href="#">Link 2</a>\
+      <a href="#">Link 3</a>\
+    </div>\
+  </li>\
+  <li class="dropdown">\
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>\
+    <div class="dropdown-content">\
+      <a href="#">Link 1</a>\
+      <a href="#">Link 2</a>\
+      <a href="#">Link 3</a>\
+    </div>\
+  </li>\
+  <li class="right"><a href="inquiry.html">Send Inquiry</a></li>\
+  <li class="right"><a href="contactus.html">Contact Us</a></li>\
+</ul>\
+</div>\
+</div>\
+</div>\
+\
+\
+');
